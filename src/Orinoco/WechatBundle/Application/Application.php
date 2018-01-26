415730ea811218e831cab23812471043b395bf54
Hailong Zhao <hailongzh@hotmail.com>
2018-01-26 07:07:00 -0500

    Use the packagist package instead.

--- src/Orinoco/WechatBundle/Application/Application.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace Orinoco\WechatBundle\Application;
-
-class Application
-{
-}
--- src/Orinoco/WechatBundle/OrinocoWechatBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Orinoco\WechatBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class OrinocoWechatBundle extends Bundle
-{
-}
