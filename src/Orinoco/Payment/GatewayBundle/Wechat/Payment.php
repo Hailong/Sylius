4da2791ef0acd8704b7cf9e58e04807a9e0feeaf
Hailong Zhao <hailongzh@hotmail.com>
2018-03-22 12:41:07 -0400

    Comment out the cert path parameters.

--- src/Orinoco/Payment/GatewayBundle/Wechat/Payment.php
+++ src/Orinoco/Payment/GatewayBundle/Wechat/Payment.php
@@ -17,8 +17,8 @@ class Payment
             'key'                => $key,   // API 密钥
 
             // 如需使用敏感接口（如退款、发送红包等）需要配置 API 证书路径(登录商户平台下载 API 证书)
-            'cert_path'          => $cert_path, // XXX: 绝对路径！！！！
-            'key_path'           => $key_path,      // XXX: 绝对路径！！！！
+            // 'cert_path'          => $cert_path, // XXX: 绝对路径！！！！
+            // 'key_path'           => $key_path,      // XXX: 绝对路径！！！！
 
             'notify_url'         => '',     // 你也可以在下单时单独设置来想覆盖它
         ];
